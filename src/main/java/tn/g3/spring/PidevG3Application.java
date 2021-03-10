7463b73b93586363b0459c32f5b271cb8a58d837
soniahadouej <sonia.hadouej@esprit.tn>
2021-03-10 23:37:17 +0100

    sonia's first commit

---
+++ src/main/java/tn/g3/spring/PidevG3Application.java
@@ -0,0 +1,13 @@
+package tn.g3.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevG3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidevG3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tn/g3/spring/PidevG3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.g3.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidevG3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
