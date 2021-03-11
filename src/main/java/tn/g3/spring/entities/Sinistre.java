9f28bba02bede50d6531ebe448d2f3b5dfc6bf72
soniahadouej <sonia.hadouej@esprit.tn>
2021-03-11 13:10:59 +0100

    sonia comment

---
+++ src/main/java/tn/g3/spring/entities/Sinistre.java
@@ -0,0 +1,16 @@
+package tn.g3.spring.entities;
+
+public class Sinistre {
+	private int idSinistre;
+
+	public int getIdSinistre() {
+		return idSinistre;
+	}
+
+	public void setIdSinistre(int idSinistre) {
+		this.idSinistre = idSinistre;
+	}
+	
+	
+
+}
